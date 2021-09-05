65e0356ae5566d29e10e14e1710436e0b047cafd
ayberkderingoz <ayberkderingoz@gmail.com>
2021-09-05 23:23:52 +0300

    playerHide eklendi
    cok iiyim

--- Assets/Scripts/Testscript.cs
+++ Assets/Scripts/Testscript.cs
@@ -10,12 +10,14 @@ using UnityEngine.UI;
 public class Testscript : MonoBehaviour
 {
     private GameObject gameobj;
-
+    private static int playerTurnIndex;
     public GameObject buttonPrefab;
+    private Button playerButton;
     
     // Start is called before the first frame update
     void Start()
     {
+        
         gameobj = GameObject.Find("Content");
         
         
@@ -27,7 +29,7 @@ public class Testscript : MonoBehaviour
             button.transform.GetChild(0).GetComponent<TMP_Text>().text = NameSceneController.oyuncuList[i].Name;
             button.GetComponent<Button>().onClick.AddListener(onClickEvent);
         }
-
+        HidePlayerButton();
         GameObject.Find("ScrollArea").GetComponent<ScrollRect>().verticalNormalizedPosition = 1;
     }
 
@@ -42,5 +44,28 @@ public class Testscript : MonoBehaviour
         else
             SceneManager.LoadScene("StartNightScene");
         //Debug.Log(GeneralMethod.GetPlayerByName(EventSystem.current.currentSelectedGameObject.transform.GetChild(0).GetComponent<TMP_Text>().text).role.ToString());
+    }
+
+    public void HidePlayerButton()
+    {
+        for (int i = 0; i < NameSceneController.oyuncuList.Count; i++)
+        {
+            playerButton = GameObject.Find("Content").transform.GetChild(i).GetComponent<Button>();
+            if (NameSceneController.oyuncuList[playerTurnIndex].Name == playerButton.transform.GetChild(0).GetComponent<TMP_Text>().text)
+            {
+                playerButton.gameObject.SetActive(false);
+            }
+            
+        }
+        playerTurnIndex++;
+        if (playerTurnIndex == NameSceneController.oyuncuList.Count)
+        {
+            playerTurnIndex = 0;
+        }
+    }
+
+    public void HideVampires(int playerTurn)
+    {
+        //Eklenicek
     }
 }
